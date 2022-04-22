b191729e388ee8c1025ee830a9c1d24ba947265b
SnehaDharne <55712542+SnehaDharne@users.noreply.github.com>
2022-04-22 15:24:15 +0530

    final final

--- core/urls.py
+++ core/urls.py
@@ -18,14 +18,13 @@ app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('TatBookStore/', TatBookStoreView, name='tat'),
-    path('PoliticsAndProse/', PolBookStoreView, name='pol'),
+    path('Politics/', TatBookStoreView, name='tat'),
+    path('Mystery/', PolBookStoreView, name='pol'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path("search/", SearchResultsView.as_view(), name='search_results'),
-
     path('add-coupon/', AddCouponView.as_view(), name='add-coupon'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
     path('remove-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart'),
